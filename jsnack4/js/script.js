170b8c8ce407d210ba3c6221f500a0177be4c86f
FrancescoCataletto <catalettofrancesco@gmail.com>
2022-04-12 19:52:55 +0200

    array created

--- jsnack4/js/script.js
+++ jsnack4/js/script.js
@@ -5,3 +5,42 @@
  chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa.
 
 */
+
+const guestArr = [
+    "Domenico Agostino",
+    "Mihai Badia",
+    "Riccardo Becattini",
+    "Manuel Benini",
+    "Bruno Bernardini",
+    "Guido Buono",
+    "Pierluigi Cancellaro",
+    "Francesco Cataletto",
+    "Stefano Cravotta",
+    "Giovanni Crispino",
+    "Giancarlo Croce",
+    "Enrico De Stefano",
+    "Silvia Dugato",
+    "Carlo Duminuco Del Monte",
+    "Francesco Ercoli",
+    "Luca Gaddini",
+    "Marco Gagliano",
+    "Cristina Gasperini",
+    "Gabriele Giusti",
+    "Chiara Grillo",
+    "Manuela Ienuso",
+    "Michele Iliescu",
+    "Ketevan Jorjoliani",
+    "Renato Lami",
+    "Davide Manciucca",
+    "Emanuele Mastronardi",
+    "Luca Mazzarini",
+    "Daniele Monteleone",
+    "Salvatore Olivieri",
+    "Leonardo Pellegrini",
+    "Agostino Piquè",
+    "Davide Pizzolla",
+    "Antonio Pollo",
+    "Ivan Russo",
+    "Amanjit Singh",
+    "Sebastiano Visco"
+    ];
