3304566a8b5d7916d9caf1822437a182131b4b2c
FrancescoCataletto <catalettofrancesco@gmail.com>
2022-04-12 17:43:59 +0200

    declared variables

--- jsnack2/js/script.js
+++ jsnack2/js/script.js
@@ -3,5 +3,9 @@
  JSnack 2
  L’utente inserisce due parole in successione, con due prompt.
  Il software stampa prima la parola più corta, poi la parola più lunga.
- 
+
 */
+
+const firstWord = prompt("Type your first word");
+const secondWord = prompt("Type your second word");
+
