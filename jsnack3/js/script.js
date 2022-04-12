e793e87f123e3f506e2571b7ee2745b9f5a07caf
FrancescoCataletto <catalettofrancesco@gmail.com>
2022-04-12 17:56:55 +0200

    declared variable

--- jsnack3/js/script.js
+++ jsnack3/js/script.js
@@ -4,5 +4,8 @@
  Il software deve chiedere per 5 volte all’utente di inserire un numero.
  Il programma stampa la somma di tutti i numeri inseriti.
  Esegui questo programma in due versioni, con il for e con il while (facoltativo)
- 
+
 */
+
+const typeNum = parseInt(prompt("Type a number"));
+
