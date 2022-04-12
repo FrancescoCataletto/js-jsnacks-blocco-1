272b39b96139ba6fe390e5cb062b5fdb27ee8f66
FrancescoCataletto <catalettofrancesco@gmail.com>
2022-04-12 17:28:37 +0200

    declared variables

--- jsnack1/js/script.js
+++ jsnack1/js/script.js
@@ -2,5 +2,11 @@
 
  JSnack 1
  L’utente inserisce due numeri in successione, con due prompt. Il software stampa il maggiore
- 
+
 */
+
+// VARIABLES TO ASK THE TWO NUMBERS
+const firstNum = parseInt(prompt("Type first number"));
+const secondNum = parseInt(prompt("Type second number"));
+
+console.log(firstNum, secondNum);
