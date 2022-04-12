1c351374b498024d4af475b3bf8e6f4fd2403e7d
FrancescoCataletto <catalettofrancesco@gmail.com>
2022-04-12 17:44:24 +0200

    completed conditions of the program

--- jsnack2/js/script.js
+++ jsnack2/js/script.js
@@ -9,3 +9,10 @@
 const firstWord = prompt("Type your first word");
 const secondWord = prompt("Type your second word");
 
+if(firstWord.length > secondWord.length){
+    console.log(`La parola più lunga è ${firstWord}, la parola più corta è ${secondWord}`)
+}else if(secondWord.length > firstWord.length){
+    console.log(`La parola più lunga è ${secondWord}, la parola più corta è ${firstWord}`)
+}else if( firstWord.length === secondWord.length){
+    console.log("Please insert two words of different length");
+}
