b2f2c4ff1c2c07bf2051c337aa3f6aae1c0fe69f
FrancescoCataletto <catalettofrancesco@gmail.com>
2022-04-13 18:52:54 +0200

    "for" loop working well

--- jsnack8/js/script.js
+++ jsnack8/js/script.js
@@ -11,8 +11,13 @@
 
 const userNum = prompt("Inserisci un numero di 4 cifre");
 
+let numSum = 0;
+
 if(userNum.length === 4){
-    console.log(parseInt(userNum));
+    for(let i = 0; i < userNum.length; i++){
+        numSum += parseInt(userNum[i]);
+        console.log(numSum);
+    }
 }else{
     console.log("Inserire un numero di 4 cifre. ")
 }
