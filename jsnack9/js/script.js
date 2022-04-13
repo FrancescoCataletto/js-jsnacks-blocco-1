9a99e72a988a7fae6cc8ca119e1354355e021889
FrancescoCataletto <catalettofrancesco@gmail.com>
2022-04-13 19:18:42 +0200

    declared variables and elements from html

--- jsnack9/js/script.js
+++ jsnack9/js/script.js
@@ -2,5 +2,16 @@
 
  JSnack 9
  Calcola la somma e la media dei primi 10 numeri.
- 
+
 */
+
+let userNums;
+
+let i = 0;
+
+let numSum = 0;
+let numMedian = 0;
+
+const sumOfNum = document.getElementById("num-sum");
+const medianOfNum = document.getElementById("num-median");
+
